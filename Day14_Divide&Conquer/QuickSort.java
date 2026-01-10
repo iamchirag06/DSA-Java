8f380dba9914f0743018ef799124b5f67a903959
Chirag Singh <singhchirag494@gmail.com>
2026-01-10 13:51:59 +0530

    DSA JAVA

---
+++ Day14_Divide&Conquer/QuickSort.java
@@ -0,0 +1,44 @@
+public class QuickSort {
+    public static void printArr(int arr[]){
+        for(int i = 0; i<arr.length;i++){
+            System.out.print(arr[i]+" ");
+        }
+        System.out.println(" ");
+    }
+    public static void quickSort(int arr[],int si,int ei){
+        //base case
+        if(si>=ei){
+            return;
+        }
+
+        //last element
+        int pIdx = partition(arr,si,ei);
+        quickSort(arr, si, pIdx-1); //left part
+        quickSort(arr, pIdx+1, ei); //right part
+    }
+    public static int partition(int arr[],int si,int ei){
+        int pivot = arr[ei];
+        int i = si -1; // to make place for elements smaller than pivot
+
+        for(int j =si; j<ei;j++){
+            if(arr[j] <= pivot){
+                i++;
+                int temp = arr[j];
+                arr[j] = arr[i];
+                arr[i] = temp;
+            }
+        }
+        i++;
+        //Last swap of Pivot Element form last to its Position
+        int temp = pivot;
+        arr[ei] = arr[i];  // change here with the help of Pivot Index not Pivot variable 
+        arr[i] = temp;
+        return i;
+    }
+    public static void main(String[] args) {
+        int arr[] ={6,3,9,8,5,2,-9};
+        quickSort(arr, 0, arr.length-1);
+        printArr(arr);
+
+    }
+}
